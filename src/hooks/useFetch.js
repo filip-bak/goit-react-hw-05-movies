f9050ee4901d22982084d482a7d526b4d37cb125
Filip Bączkowski <filo.b@wp.pl>
2023-07-31 01:25:59 +0200

    feat: add fetch api hook

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,42 +1,69 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import axios from 'axios';
 
 axios.defaults.baseURL = 'https://api.themoviedb.org/3';
 
-const useFetch = ({ url, params, array }) => {
+const useFetch = ({ url, initialParams = {} }) => {
   const [data, setData] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
+  const [params, updateParams] = useState(initialParams);
+  const prevQueryRef = useRef(null);
 
   const allParams = new URLSearchParams({
-    api_key: '8ffc049be01c9ac683da541d3958668c',
+    api_key: process.env.REACT_APP_API_KEY,
     ...params,
   });
 
+  const isSameData = prevQueryRef?.current === params?.query;
+
   useEffect(() => {
+    const controller = new AbortController();
+    const signal = controller.signal;
+
+    setIsLoading(true);
     const fetchData = async () => {
-      const response = await axios.get(`${url}?${allParams}`);
-      setData(response.data);
+      try {
+        const response = await axios.get(`${url}?${allParams}`, { signal });
+        console.log('API: ', response.data);
+        setData(response.data);
+        prevQueryRef.current = params?.query;
+      } catch (err) {
+        if (err.name === 'CanceledError') {
+          setIsLoading(true);
+          console.log('Request aborted');
+        } else {
+          setError(err);
+          console.log(err);
+          setData(null);
+        }
+      } finally {
+        setIsLoading(false);
+      }
     };
 
-    try {
-      setIsLoading(true);
-      if (params?.query === '') {
-        setData(null);
-        return;
-      }
-      fetchData();
-    } catch (e) {
-      setError(e);
+    // if search query is "" then reset data and skip fetch request
+    if (!url || params?.query === '') {
       setData(null);
-    } finally {
+      prevQueryRef.current = null;
       setIsLoading(false);
+      return;
+    }
+    // if data is the same as previous, skip fetch request
+    if (isSameData) {
+      setIsLoading(false);
+      return;
     }
 
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [url, array]);
+    fetchData();
 
-  return { data, isLoading, error };
+    return () => {
+      controller?.abort();
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [url, params]);
+
+  return { data, error, isLoading, updateParams };
 };
 
 export default useFetch;
